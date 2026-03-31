2d5c258d861f8f92f0eac581763721a145d838e2
Pim Vermeer <pim.vermeer@gmail.com>
2026-03-31 09:26:04 +0000

    chore(dev): add build all commands for rpm-tool

--- .devcontainer/install-deps.sh
+++ .devcontainer/install-deps.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Dev
-sudo dnf install -y cmake ninja-build gcc g++ mesa-libgbm-devel libappindicator-gtk3-devel openssl-devel libcurl-devel miniupnpc-devel libdrm-devel libva-devel libnotify-devel nodejs npm libevdev-devel opus-devel pulseaudio-libs-devel numactl-devel libcap-devel pipewire-devel micromamba vulkan-devel glslc
+sudo dnf install -y cmake ninja-build gcc g++ micromamba mesa-libgbm-devel libappindicator-gtk3-devel openssl-devel libcurl-devel miniupnpc-devel libdrm-devel libva-devel libnotify-devel nodejs npm libevdev-devel opus-devel pulseaudio-libs-devel numactl-devel libcap-devel pipewire-devel vulkan-devel glslc
 
 # External dev
 mamba create -y -p .mamba/cuda-env conda-forge::cuda-nvcc
